89eaef708f3fadbe53e9b3192fcb7d1539d728a1
Solomakha Dmitry <solomakhadmitry@gmail.com>
2025-02-18 19:25:27 +0200

    Initial Commit

---
+++ WebApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebApp
+//
+//  Created by Дмитрий Соломаха on 18.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
