4bbe40d7b905935146df692a578db7233c3f6827
Solomakha <solomakhadmitry@gmail.com>
2025-06-02 15:05:47 +0300

    Added webView.  Added constraints. Deleted unnecessary comments.

--- WebApp/ViewController.swift
+++ WebApp/ViewController.swift
@@ -1,19 +1,32 @@
-//
-//  ViewController.swift
-//  WebApp
-//
-//  Created by Дмитрий Соломаха on 18.02.2025.
-//
-
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    let webView = WKWebView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(webView)
+        guard let url = URL(string: "https://www.youtube.com/watch?v=JafGypqFvs4&t=205s") else { return }
+        webView.load(URLRequest(url: url))
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        addConstraints()
+    }
+    
+    func addConstraints() {
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            webView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            webView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+    }
+    
 }
-
